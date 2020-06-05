736490774fa8702c0528ed0c4e5a7069a1a64c48
Niall Larson <nialldizzle@gmail.com>
2020-06-04 19:40:00 -0700

    writeToFile

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
+const inquirer = require("inquirer")
 
 const questions = [
     {
-        message: 'What is your github username?',
-        name: 'username',
+        message: "What is your github username?",
+        name: "username",
     },
     {
-        message: 'What is the name of your github repository?',
-        name: 'repository',
+        message: "What is the name of your github repository?",
+        name: "repository",
     }
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile("readme.md", data, function(err){
+        if(err){
+            return console.log(err);
+        }
+    })
 }
 
 function init() {
