8d85576fc45889be1f8458829ed06f5e0de7fee6
Niall Larson <nialldizzle@gmail.com>
2020-06-04 19:27:35 -0700

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,14 @@
-const questions = [
+const fs = require('fs');
 
+const questions = [
+    {
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        message: 'What is the name of your github repository?',
+        name: 'repository',
+    }
 ];
 
 function writeToFile(fileName, data) {
